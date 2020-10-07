c2c2b08c4bd168645b4f5511e6f305f3f70120a3
Praneethachodey <praneethachodey@gmail.com>
2020-10-07 09:56:54 +0530

    [Praneetha]Added: ability to choose a letter by player

--- src/com/bridgelabz/practice/TicTacToeGame.java
+++ src/com/bridgelabz/practice/TicTacToeGame.java
@@ -1,24 +1,36 @@
 package com.bridgelabz.practice;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
-	
-	public char[] createBoard()
-	{
+
+	// uc1-creating board and returning it
+	public static char[] createBoard() {
 		char[] board = new char[10];
-		for(int i =1;i<=8;i++)
-		{
-			board[i]=' ';
+		for (int position = 1; position <= 9; position++) {
+			board[position] = ' ';
 		}
 		return board;
 	}
 
-	public static void main(String[] args) {
-		TicTacToeGame t = new TicTacToeGame();
-		char[] board = t.createBoard();
-		for(int i = 1;i<=9;i++)
-		{
-			System.out.println(board[i]);
+	// uc2-ability to choose a letter for the player
+	public static char chooseLetter() {
+		System.out.println("choose X or O");
+		Scanner scan = new Scanner(System.in);
+		String letter = scan.next();
+		while (!letter.equalsIgnoreCase("x") && !letter.equalsIgnoreCase("o")) {
+			System.out.println("please enter correct letter");
+			scan = new Scanner(System.in);
+			letter = scan.next();
 		}
+		scan.close();
+		return letter.charAt(0);
+	}
+
+	public static void main(String[] args) {
+		char[] board = createBoard();
+		char letter = chooseLetter();
+		System.out.println("letter chosen by player : " +letter);
 	}
 
 }
